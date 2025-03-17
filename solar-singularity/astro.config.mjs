9b41547334642509e52e493bee16c29935ddfaab
Chriss Furenes <chrissof@uia.no>
2025-03-17 03:37:59 +0100

    some stuff

--- solar-singularity/astro.config.mjs
+++ solar-singularity/astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://ctf.chrissolavfurenes.com',
+    site: 'https://chrissfurenes.github.io',
+    base: 'DDC-CFT-2025-WriteUp',
 });
