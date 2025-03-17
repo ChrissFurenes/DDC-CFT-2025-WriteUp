26a811ca85c7c59d85771d941b63e690067fff55
Chriss Furenes <chrissolfu@gmail.com>
2025-03-17 10:22:08 +0100

    added UI

--- solar-singularity/astro.config.mjs
+++
@@ -1,8 +0,0 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
-export default defineConfig({
-    site: 'https://chrissfurenes.github.io',
-    base: 'DDC-CTF-2025-WriteUp',
-});
